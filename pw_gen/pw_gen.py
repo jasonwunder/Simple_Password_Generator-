482a760555550aa6513cc2d2c9308fe7be79c3e7
Jason Wunder <jasonwunder94@gmail.com>
2022-03-08 10:08:23 -0800

    updated

--- pw_gen/pw_gen.py
+++ pw_gen/pw_gen.py
@@ -1,35 +1,47 @@
+#!/usr/bin/env python3
 
 import random
 
-# set the variables on what is included in the passwords
-uppercase_letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-lowercase_letters = uppercase_letters.lower()
-digits = "0123456789"
-symbols = "!#$%&()*+,-./:;<=>?@[\\]^_`{|}~"
+# fucntion that lets the user choose the options to use for the generatior 
+def options():
 
-# set the booleans for the password rules 
-upper = True
-lower = True
-numbers = True
-syms = True
 
-# create a string containing all the you will use
-all = ""
+def pw_gen():
+    # set the variables on what is included in the passwords
+    uppercase_letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    lowercase_letters = uppercase_letters.lower()
+    digits = "0123456789"
+    symbols = "!#$%&()*+,-./:;<=>?@[\\]^_`{|}~"
 
-if upper:
-    all += uppercase_letters
-if lower:
-    all += lowercase_letters
-if numbers:
-    all += digits
-if syms:
-    all += symbols
+    # set the booleans for the password rules 
+    upper = True
+    lower = True
+    numbers = True
+    syms = True
 
-# set the length of the passwords and the amount of passwords to be generated
-length = 25
-amount = 10
+    # create a string containing all the you will use
+    all = ""
 
-# join the all string with a empty string to create the passwords. then print the new variable
-for passwd in range(amount):
-    password = "".join(random.sample(all, length))
-    print(password)
+    if upper:
+        all += uppercase_letters
+    if lower:
+        all += lowercase_letters
+    if numbers:
+        all += digits
+    if syms:
+        all += symbols
+
+    # set the length of the passwords and the amount of passwords to be generated
+    length = 25
+    amount = 10
+
+    # join the all string with a empty string to create the passwords. then print the new variable
+    for passwd in range(amount):
+        password = "".join(random.sample(all, length))
+    return password
+
+def main():
+    pw_gen()
+
+if __name__ == "__main__":
+    main()
